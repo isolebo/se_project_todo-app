d4de7938d529b8b3804b26678e8f18bcc97550fd
Isiaq Solebo <i.solebo32@gmail.com>
2026-01-31 21:28:09 -0600

    update changes in idex.js and created Popuup.js and PopupWithForm.js

--- components/section.js
+++ components/section.js
@@ -1,19 +1,19 @@
 class Section {
-  constructor({ items, renderer, containSelector }) {
+  constructor({ items, renderer, containerSelector }) {
     this._items = items;
     this._renderer = renderer;
-    this._container = document.querySelector(containSelector);
+    this._container = document.querySelector(containerSelector);
   }
 
   renderItems() {
-    this._items.forEach((item) => {
-      this._renderer(item);
+    this._items.forEach((items) => {
+      this._renderer(items);
     });
   }
 
-  // addItem(element) {
-  //   this._container.append(element);
-  // }
+  addItem(element) {
+    this._container.append(element);
+  }
 }
 
 export default Section;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,26 @@ import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/section.js";
+import PopupWithForm from "../components/PopupWithForm.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
-const addTodoPopup = document.querySelector("#add-todo-popup");
-const addTodoForm = addTodoPopup.querySelector(".popup__form");
-const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
+const addTodoPopupEl = document.querySelector("#add-todo-popup");
+const addTodoForm = addTodoPopupEl.querySelector(".popup__form");
+const addTodoCloseBtn = addTodoPopupEl.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+const addTodoPopup = new PopupWithForm({
+  popupSelector: "#add-todo-popup",
+  handleFormSubmit: () => {},
+});
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  return todo.getView();
+};
+const renderTodo = (items) => {
+  const todoElement = generateTodo(items);
+  section.addItem(todoElement);
+};
 
 const section = new Section({
   items: initialTodos, //pass inital todo
@@ -18,7 +32,7 @@ const section = new Section({
 
   containerSelector: ".todos__list",
 });
-section.renderItems(items);
+section.renderItems();
 //call section instances render method
 
 const openModal = (modal) => {
@@ -29,17 +43,12 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-const generateTodo = (data) => {
-  const todo = new Todo(data, "#todo-template");
-  return todo.getView();
-};
-
 addTodoButton.addEventListener("click", () => {
-  openModal(addTodoPopup);
+  openModal(addTodoPopupEl);
 });
 
 addTodoCloseBtn.addEventListener("click", () => {
-  closeModal(addTodoPopup);
+  closeModal(addTodoPopupEl);
 });
 
 addTodoForm.addEventListener("submit", (evt) => {
@@ -54,7 +63,7 @@ addTodoForm.addEventListener("submit", (evt) => {
   const values = { name, date, id };
   const todo = generateTodo(values);
   todosList.append(todo);
-  closeModal(addTodoPopup);
+  closeModal(addTodoPopupEl);
 });
 
 const newTodoValidator = new FormValidator(validationConfig, addTodoForm);
